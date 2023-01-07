29f4372d3b7d8a6ef336d1a18b107706436fc0ab
rim_personal_laptop <hr0294@nate.com>
2023-01-07 23:22:55 +0900

    Additional preprocessing

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,6 +40,10 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
     df.info()
 
+    # Drop outliers(2)
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Save as CSV")
     # Save output as .csv file
     df.to_csv(args.output_artifact, index=False)
